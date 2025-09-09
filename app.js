16416c711158ac44f338f85a761d2628f43e58b2
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-09 19:45:57 +0200

    js commit

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino');
     const grid = document.querySelector('.grid');
 
-    function control() {
+    function control(e) {
+        if (e.code === 'Space') {
+            jump();
+        }
+    }
+
+    let position = 0;
+
+    function jump() {
+        let timerID = setInterval(function() {
+
+            // move up
+            position += 30;
+            dino.style.bottom = position + 'px';
+        }, 20);
 
     }
+
+
     document.addEventListener('keyup', control);
 });
