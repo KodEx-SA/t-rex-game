928092c8b15458a82df1457362c60fa934df1a73
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-09 19:37:01 +0200

    app js commit

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dino = document.querySelector('.dino');
+    const grid = document.querySelector('.grid');
+
+    function control() {
+
+    }
+    document.addEventListener('keyup', control);
+});
